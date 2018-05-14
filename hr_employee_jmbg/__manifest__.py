b8d6d979afa947980c04b03b79e740f8c35d0fc0
chekfu <chekfu20@gmail.com>
2018-05-14 17:04:39 +0200

    kreiranje početnih postavki

---
+++ hr_employee_jmbg/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hr_employee_jmbg",
+
+    'summary': """
+        JMBG field in HR employee
+        """,
+
+    'description': """
+        searchable unique JMBG number for ID
+    """,
+
+    'author': "Marko Fuček",
+    'website': "",
+
+    'category': 'Human Resources',
+    'version': '0.1',
+
+
+    'depends': ['base'],
+
+
+    'data': [
+
+        'views/views.xml','models.py'
+
+    ],
+
+}
