e5e666a569b47fbe175b49f1a86d4c1f2719c052
chekfu <chekfu20@gmail.com>
2018-05-14 18:28:31 +0200

    inicijalni model i view

--- hr_employee_jmbg/__manifest__.py
+++ hr_employee_jmbg/__manifest__.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 {
     'name': "hr_employee_jmbg",
-
     'summary': """
         JMBG field in HR employee
         """,
-
     'description': """
-        searchable unique JMBG number for ID
+        unique JMBG number for ID
     """,
-
     'author': "Marko Fuček",
     'website': "",
-
     'category': 'Human Resources',
     'version': '0.1',
-
-
-    'depends': ['base'],
-
-
+    'depends': ['hr'],
     'data': [
 
-        'views/views.xml','models.py'
+        'views/views.xml',
 
     ],
+     'installable': True,
+
 
 }
